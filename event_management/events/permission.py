aa570b7b365a454ddab4c5c635794bfa397fb391
poonam saxena <saxena2111poonam@gmail.com>
2025-02-20 19:30:15 +0530

    create the Event Participants and invitation view

--- event_management/events/permission.py
+++ event_management/events/permission.py
@@ -1,8 +1,21 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
-
+from.models import Event
 
 class My_Permission(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         return obj.host == request.user
+
+
+    
+class HostListPermission(BasePermission):
+    def has_permission(self, request, view):
+        event_id = view.kwargs.get("event_id")
+        if not event_id:
+            return False
+        try:
+            event = Event.objects.get(id=event_id)
+            return event.host == request.user
+        except Event.DoesNotExist:
+            return False
